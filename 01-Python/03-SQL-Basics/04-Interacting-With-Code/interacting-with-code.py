dd85ab3b14882e78ddbd54b6ffb647b592d8748c
GaetanDuron <gaetan.duron@gmail.com>
2019-10-09 11:57:36 +0200

    add a solution file to the exercise (not correct)
    Former-commit-id: f4d21be0d1262e2f595a958483b4902622413ad6

--- 01-Python/03-SQL-Basics/04-Interacting-With-Code/interacting-with-code.py
+++ 01-Python/03-SQL-Basics/04-Interacting-With-Code/interacting-with-code.py
@@ -1,48 +1,22 @@
-import unittest
 import sqlite3
 
-conn = sqlite3.connect('data/ecommerce.db') #??????
-db = conn.cursor()
-
-def artist_count(db): #to_refacto
-  # TODO: use `db` to execute an SQL query against the database.
-
-    request = '''SELECT COUNT(*) FROM artists
-    '''
-    results = db.execute(request)
-    return results
+def directors_count(db):
+  # TO-DO: use 'db' to execute an SQL query against the database.
+  # Return directors count in database.
+  pass
 
 def number_of_rows(db, table_name):
-  # TODO: count number of rows in table table_name
+  # TO-DO: count number of rows in table table_name
+  pass
 
-    request = '''SELECT COUNT(*) FROM #{table_name}
-    '''
-    results = db.execute(request)
-    return results
+def sorted_directors(db):
+  # TO-DO: return a list of directors' names sorted alphabetically
+  pass
 
-def sorted_artists(db):
-  # TODO: return array of artists' names sorted alphabetically
+def love_movies(db):
+  # TO-DO: return a list of love movies' names
+  pass
 
-    request = '''SELECT Name FROM artists ORDER BY Name
-    '''
-    results = db.execute(request)
-    return results
-
-def love_tracks(db):
-  # TODO: return array of love songs' names
-
-    request = '''SELECT Name FROM tracks WHERE Name LIKE '%love%' ORDER BY Name
-    '''
-    results = db.execute(request)
-    return results
-
-def long_tracks(db, min_length):
-  # TODO: return an array of tracks' names verifying: duration > min_length (minutes) sorted by length (ascending)
-
-    request = '''
-    SELECT Name FROM tracks
-    WHERE milliseconds > #{min_length} * 60000
-    ORDER BY milliseconds ASC
-    '''
-    results = db.execute(request)
-    return results
+def long_movies(db, min_length):
+  # TO-DO: return a list of movies' names verifying: minutes > min_length, sorted by length (ascending)
+  pass
