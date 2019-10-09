b9afb0c9c3059c2b7831135f07b64421c998f060
GaetanDuron <gaetan.duron@gmail.com>
2019-10-09 13:53:32 +0200

    rename file with - to _
    Former-commit-id: 255bfb8455262be8a1e4d03f16b27b437918b21e

---
+++ 01-Python/03-SQL-Basics/04-Interacting-With-Code/interacting_with_code.py
@@ -0,0 +1,21 @@
+def directors_count(db):
+    # TO-DO: use 'db' to execute an SQL query against the database.
+    # Return directors count in database.
+    pass
+
+def number_of_rows(db, table_name):
+    # TO-DO: count number of rows in table table_name
+    pass
+
+def sorted_directors(db):
+    # TO-DO: return a list of directors' names sorted alphabetically
+    pass
+
+def love_movies(db):
+    # TO-DO: return a list of love movies' names
+    pass
+
+def long_movies(db, min_length):
+    # TO-DO: return a list of movies' names
+    # verifying: minutes > min_length, sorted by length (ascending)
+    pass
--- 01-Python/03-SQL-Basics/04-Interacting-With-Code/interacting_with_code_solution.py
+++ 01-Python/03-SQL-Basics/04-Interacting-With-Code/interacting_with_code_solution.py
@@ -1,40 +1,35 @@
-import sqlite3
-
-conn = sqlite3.connect('data/movies.sqlite')
-db = conn.cursor()
-
 def directors_count(db):
-  # TO-DO: use 'db' to execute an SQL query against the database.
-  # Return directors count in database.
+    # TO-DO: use 'db' to execute an SQL query against the database.
+    # Return directors count in database.
 
-    request = 'SELECT COUNT(*) FROM directors'
+    request = 'SELECT COUNT(*) FROM directors;'
     results = db.execute(request)
-    return results
+    return results.fetchone()[0]
 
 def number_of_rows(db, table_name):
-  # TO-DO: count number of rows in table table_name
+    # TO-DO: count number of rows in table table_name
 
     request = f'SELECT COUNT(*) FROM {table_name}'
     results = db.execute(request)
-    return results
+    return results.fetchall()
 
 def sorted_directors(db):
-  # TO-DO: return a list of directors' names sorted alphabetically
+    # TO-DO: return a list of directors' names sorted alphabetically
 
     request = 'SELECT name FROM directors ORDER BY name'
     results = db.execute(request)
     return results
 
 def love_movies(db):
-  # TO-DO: return a list of love movies' names
+    # TO-DO: return a list of love movies' names
 
     request = 'SELECT title FROM movies WHERE title LIKE "%love%" ORDER BY title'
     results = db.execute(request)
     return results
 
 def long_movies(db, min_length):
-  # TO-DO: return a list of movies' names
-  # verifying: minutes > min_length, sorted by length (ascending)
+    # TO-DO: return a list of movies' names
+    # verifying: minutes > min_length, sorted by length (ascending)
 
     request = '\
       SELECT title FROM movies\
