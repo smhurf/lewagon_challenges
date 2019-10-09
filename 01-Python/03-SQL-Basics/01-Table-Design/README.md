e91f2d4f98b717ffa9c43760ee1ac790d8d8a2ba
Thibault Pedelhez <thi.pedelhez@gmail.com>
2019-10-09 18:18:48 +0200

    exercice 1,2,3
    Former-commit-id: 238408f1d514c750869ac8a74fcb4d75089a184a

--- 01-Python/03-SQL-Basics/01-Table-Design/README.md
+++ 01-Python/03-SQL-Basics/01-Table-Design/README.md
@@ -6,18 +6,19 @@ The goal of this first challenge is to create our first table using the [SQL Des
 
 #### Designing our first table
 
-Let's create a table to store users. The table should have these columns:
+Let's create a table to store customers. The table should have these columns:
 
 - my_name
 - Last name
 - Email
+- City
 
 NOTE: Remember, all of our tables will also have an `id` column!
 
 #### Drawing the table
 
-Use the [SQL Designer](http://db.lewagon.com) to draw the `users` table with the columns specified above.
-To check your solution, click on "Save / Load", then "Save XML", copy/paste the generated XML code in `lib/users.xml`. You can then `rake` to check your solution.
+Use the [SQL Designer](http://db.lewagon.com) to draw the `customers` table with the columns specified above.
+To check your solution, click on "Save / Load", then "Save XML", copy/paste the generated XML code in `customers.xml`. You can then `make` to check your solution.
 
 ## Key learning points
 
--- 01-Python/03-SQL-Basics/02-DB-Design/tests/test_user.py
+++ 01-Python/03-SQL-Basics/02-DB-Design/tests/test_user.py
@@ -8,83 +8,83 @@ class Users(unittest.TestCase):
 #1 in the good folders
 
 #2 should have a survey table
-    def test_surveys_table_exists(self):
-        with open('surveys.xml', 'r') as file:
+    def test_customers_table_exists(self):
+        with open('ecommerce.xml', 'r') as file:
           data = file.read().replace('\n', '')
         soup = BeautifulSoup(data, 'xml')
         tables = soup.find_all('table')
         t = []
         for table in tables:
            t.append(table['name'])
-        self.assertEqual('surveys' in t, True)
+        self.assertEqual('customers' in t, True)
 
 #2' should have user_id, foreign_key?
-    def test_surveys_should_have_the_correct_fields(self):
-        with open('surveys.xml', 'r') as file:
+    def test_customers_should_have_the_correct_fields(self):
+        with open('ecommerce.xml', 'r') as file:
             data= file.read().replace('\n', '')
         soup = BeautifulSoup(data, 'xml')
-        table = soup.find_all('table', attrs={"name": "surveys"})
+        table = soup.find_all('table', attrs={"name": "customers"})
         for row in table[0].find_all('row'):
-            self.assertEqual(row.get('name') in ['user_id', 'title', 'id'], True)
+            self.assertEqual(row.get('name') in ['first_name', 'last_name', 'email', 'city', 'id'], True)
 
-#3 should have a question table
-    def test_questions_table_exists(self):
-        with open('surveys.xml', 'r') as file:
+#3 should have a product table
+    def test_products_table_exists(self):
+        with open('ecommerce.xml', 'r') as file:
           data = file.read().replace('\n', '')
         soup = BeautifulSoup(data, 'xml')
         tables = soup.find_all('table')
         t = []
         for table in tables:
            t.append(table['name'])
-        self.assertEqual('questions' in t, True)
+        self.assertEqual('products' in t, True)
 
 #3' should have survey_id, foreign_key
-    def test_questions_should_have_the_correct_fields(self):
-        with open('surveys.xml', 'r') as file:
+    def test_products_should_have_the_correct_fields(self):
+        with open('ecommerce.xml', 'r') as file:
             data= file.read().replace('\n', '')
         soup = BeautifulSoup(data, 'xml')
-        table = soup.find_all('table', attrs={"name": "questions"})
+        table = soup.find_all('table', attrs={"name": "products"})
         for row in table[0].find_all('row'):
-            self.assertEqual(row.get('name') in ['survey_id', 'text', 'id'], True)
+            self.assertEqual(row.get('name') in ['name', 'unit_price', 'id'], True)
 
-#4 should have an anwser table
-    def test_answers_table_exists(self):
-        with open('surveys.xml', 'r') as file:
+#4 should have an orders table
+    def test_orders_table_exists(self):
+        with open('ecommerce.xml', 'r') as file:
           data = file.read().replace('\n', '')
         soup = BeautifulSoup(data, 'xml')
         tables = soup.find_all('table')
         t = []
         for table in tables:
            t.append(table['name'])
-        self.assertEqual('answers' in t, True)
+        self.assertEqual('orders' in t, True)
 
 #4' should have anwser_id, foreign_key
-    def test_answers_should_have_the_correct_fields(self):
-        with open('surveys.xml', 'r') as file:
+    def test_orders_should_have_the_correct_fields(self):
+        with open('ecommerce.xml', 'r') as file:
             data= file.read().replace('\n', '')
         soup = BeautifulSoup(data, 'xml')
-        table = soup.find_all('table', attrs={"name": "answers"})
+        table = soup.find_all('table', attrs={"name": "orders"})
         for row in table[0].find_all('row'):
-            self.assertEqual(row.get('name') in ['question_id', 'text', 'id'], True)
+            self.assertEqual(row.get('name') in ['date', 'id_customers', 'id'], True)
 
-#5 should havee a user table
-    def test_users_table_exists(self):
-        with open('surveys.xml', 'r') as file:
+#5 should havee a product_orders table
+    def test_products_orders_table_exists(self):
+        with open('ecommerce.xml', 'r') as file:
           data = file.read().replace('\n', '')
         soup = BeautifulSoup(data, 'xml')
         tables = soup.find_all('table')
         t = []
         for table in tables:
            t.append(table['name'])
-        self.assertEqual('users' in t, True)
+        self.assertEqual('product_orders' in t, True)
 
     def test_users_should_have_the_correct_fields(self):
-        with open('surveys.xml', 'r') as file:
+        with open('ecommerce.xml', 'r') as file:
             data= file.read().replace('\n', '')
         soup = BeautifulSoup(data, 'xml')
-        table = soup.find_all('table', attrs={"name": "users"})
+        table = soup.find_all('table', attrs={"name": "product_orders"})
         for row in table[0].find_all('row'):
-            self.assertEqual(row.get('name') in ['id'], True)
+            self.assertEqual(row.get('name') in ['id_products', 'id_orders', 'quantity', 'id'], True)
 
 
 
