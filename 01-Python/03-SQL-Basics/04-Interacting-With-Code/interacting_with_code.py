2051e16c6185eaea8d6773819f299547371ebe5a
GaetanDuron <gaetan.duron@gmail.com>
2019-10-09 18:53:43 +0200

    Exo is clean
    Former-commit-id: f01f1942dda6b2bf78f76a5f4748efed45e77e17

--- 01-Python/03-SQL-Basics/04-Interacting-With-Code/interacting_with_code.py
+++ 01-Python/03-SQL-Basics/04-Interacting-With-Code/interacting_with_code.py
@@ -3,16 +3,16 @@ def directors_count(db):
     # Return directors count in database.
     pass
 
-def number_of_rows(db, table_name):
-    # TO-DO: count number of rows in table table_name
-    pass
-
 def sorted_directors(db):
     # TO-DO: return a list of directors' names sorted alphabetically
     pass
 
 def love_movies(db):
     # TO-DO: return a list of love movies' names
+    pass
+
+def directors_with_name(db, name):
+    # TO-DO: count number of director with this name
     pass
 
 def long_movies(db, min_length):
