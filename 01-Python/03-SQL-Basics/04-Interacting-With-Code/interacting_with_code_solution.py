b452a6c95d09201940d2961284d5fb48135e250f
GaetanDuron <gaetan.duron@gmail.com>
2019-10-09 17:10:21 +0200

    Wip working on test
    Former-commit-id: 918d664346245c87c48717fb24ebcdb24b32a116

--- 01-Python/03-SQL-Basics/04-Interacting-With-Code/interacting_with_code_solution.py
+++ 01-Python/03-SQL-Basics/04-Interacting-With-Code/interacting_with_code_solution.py
@@ -4,37 +4,40 @@ def directors_count(db):
 
     request = 'SELECT COUNT(*) FROM directors;'
     results = db.execute(request)
-    return results.fetchone()[0]
+    return int(results.fetchone()[0])
 
-def number_of_rows(db, table_name):
+def directors_with_name(db, name):
     # TO-DO: count number of rows in table table_name
 
-    request = f'SELECT COUNT(*) FROM {table_name}'
-    results = db.execute(request)
-    return results.fetchall()
+    results = db.execute('SELECT COUNT(*) FROM directors WHERE name=?', (name,))
+    return int(results.fetchone()[0])
 
 def sorted_directors(db):
     # TO-DO: return a list of directors' names sorted alphabetically
 
     request = 'SELECT name FROM directors ORDER BY name'
     results = db.execute(request)
-    return results
+    sql_list = results.fetchall()
+    directors_list = [val for sublist in sql_list for val in sublist]
+    return directors_list
 
 def love_movies(db):
     # TO-DO: return a list of love movies' names
 
     request = 'SELECT title FROM movies WHERE title LIKE "%love%" ORDER BY title'
     results = db.execute(request)
-    return results
+    love_movies_list = [val for sublist in results.fetchall() for val in sublist]
+    return love_movies_list
 
 def long_movies(db, min_length):
     # TO-DO: return a list of movies' names
     # verifying: minutes > min_length, sorted by length (ascending)
 
-    request = '\
+    request = f'\
       SELECT title FROM movies\
       WHERE minutes > {min_length}\
       ORDER BY minutes ASC\
     '
     results = db.execute(request)
+    results = [val for sublist in results.fetchall() for val in sublist]
     return results
