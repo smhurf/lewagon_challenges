719aa35fb620917a3b0dd8a9da79ea4aad5bc208
GaetanDuron <gaetan.duron@gmail.com>
2019-10-10 22:01:30 +0200

    Add exercise file + tests
    Former-commit-id: ea14d3652e9bacf598bed15a349f2223a82d6522

--- 01-Python/04-SQL-Advanced/03_2-Rank-And-Partition/query.py
+++ 01-Python/04-SQL-Advanced/03_2-Rank-And-Partition/query.py
@@ -1,39 +1,10 @@
 # pylint:disable=C0111,C0103
 
-def group_by_customer(db):
-    """TO DO: ..."""
-    request = '''SELECT orders.CustomerID,
-        AVG(orderdetails.UnitPrice * orderdetails.Quantity) AS AvgOrderAmount,
-        MIN(orderdetails.UnitPrice * orderdetails.Quantity) AS MinOrderAmount,
-        MAX(orderdetails.UnitPrice * orderdetails.Quantity) AS MaxOrderAmount,
-        SUM(orderdetails.UnitPrice * orderdetails.Quantity) AS TotalOrderAmount
-        FROM orders
-        JOIN orderdetails ON orders.OrderID = orderdetails.OrderID
-        GROUP BY orders.CustomerID
-    '''
-    results = db.execute(request)
-    return results
+def movie_duration_buckets(db):
+    pass
 
-def partition_by_customer(db):
-    """TO DO: ..."""
-    request = '''SELECT orders.CustomerID,
-        orderdetails.UnitPrice * orderdetails.Quantity AS OrderAmount,
-        RANK() OVER(PARTITION BY orders.CustomerID
-        ORDER BY (orderdetails.UnitPrice * orderdetails.Quantity) DESC) AS "Row Number",
-        orderdetails.UnitPrice,
-        orderdetails.Quantity
-        FROM orders
-        JOIN orderdetails ON orders.OrderID = orderdetails.OrderID
-    '''
-    results = db.execute(request)
-    return results
+def longuest_movies_by_director(db):
+    pass
 
-
-
-# results = group_by_employee(db)
-#results = rank_products(db)
-# results = rank_orders(db)
-# results = partition_by_employee(db)
-# results = results.fetchall()
-#for r in results:
-#    print(r)
+def top_3_longuest(db):
+    pass
