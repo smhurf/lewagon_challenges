dde2718944a0386234e348a9a1207d89c7b1364d
GaetanDuron <gaetan.duron@gmail.com>
2019-10-10 22:05:56 +0200

    add missing param
    Former-commit-id: f7cc2bbc3db04b47a25d2131b96d78d4ea7345c7

--- 01-Python/04-SQL-Advanced/03_2-Rank-And-Partition/query.py
+++ 01-Python/04-SQL-Advanced/03_2-Rank-And-Partition/query.py
@@ -3,8 +3,8 @@
 def movie_duration_buckets(db):
     pass
 
-def longuest_movies_by_director(db):
+def longuest_movies_by_director(db, first_letter):
     pass
 
-def top_3_longuest(db):
+def top_3_longuest(db, first_letter):
     pass
