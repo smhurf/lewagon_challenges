9d2cf8d62d2919535cfe207cb8a89adf5f9c3150
pierre collinet <pi.collinet@gmail.com>
2019-10-10 20:18:37 +0200

    test for ex01 - basic queries
    Former-commit-id: bbac93917ecd2d20fb39b55fdf7e33a1d6d81e76

--- 01-Python/04-SQL-Advanced/01-Query-the-DB/tests/test_get_waiting_time.py
+++ 01-Python/04-SQL-Advanced/01-Query-the-DB/tests/test_get_waiting_time.py
@@ -1,38 +1,26 @@
 # pylint: disable-all
 import unittest
-from query_db import get_orders_range
+from query_db import get_waiting_time
 import sqlite3
 
 conn = sqlite3.connect('db/ecommerce.db')
 db = conn.cursor()
 
-# 1. Test size list : TO DO
-# 2. Test column name : TO DO
-# 3. Test type(results = list) : TO DO
-# 4. Good results first results : TO DO
-# 5. Good results second results : TO DO
 class TestWaitingTime(unittest.TestCase):
     def test_size_list(self):
-        results = get_orders_range(db)
-        results = results.fetchall()
-        self.assertEqual(1, 2)
-
-    def test_column_name(self):
-        results = get_orders_range(db)
-        results = results.fetchall()
-        self.assertEqual(1, 2)
+        results = get_waiting_time(db)
+        self.assertEqual(len(results), 20)
 
     def test_type_results(self):
-        results = get_orders_range(db)
-        results = results.fetchall()
-        self.assertEqual(1, 2)
+        results = get_waiting_time(db)
+        self.assertIsInstance(results, list)
 
     def test_first_result(self):
-        results = get_orders_range(db)
-        results = results.fetchall()
-        self.assertEqual(1, 2)
+        results = get_waiting_time(db)
+        expected = (1, 1, 1, '2012-01-04', '2012-01-09', '2012-01-05', 1, 3.75, 1.0)
+        self.assertEqual(results[0], expected)
 
-    def test_second_result(self):
-        results = get_orders_range(db)
-        results = results.fetchall()
-        self.assertEqual(1, 2)
+    def test_last_result(self):
+        results = get_waiting_time(db)
+        expected = (19, 2, 4, '2013-02-21', '2013-02-26', '2013-03-01', 4, 14.0, 8.0)
+        self.assertEqual(results[len(results) - 1], expected)
--- 01-Python/04-SQL-Advanced/01-Query-the-DB/tests/test_query_orders.py
+++ 01-Python/04-SQL-Advanced/01-Query-the-DB/tests/test_query_orders.py
@@ -9,12 +9,10 @@ db = conn.cursor()
 class TestQueryOrders(unittest.TestCase):
     def test_length_list(self):
         results = query_orders(db)
-        results = results.fetchall()
         self.assertEqual(len(results), 20)
 
     def test_first_element(self):
         results = query_orders(db)
-        results = results.fetchall()
         result_0 = results[0]
         expected = (1, 1, 1, '2012-01-04', '2012-01-09', '2012-01-05', 1, 3.75)
         self.assertEqual(result_0, expected)
