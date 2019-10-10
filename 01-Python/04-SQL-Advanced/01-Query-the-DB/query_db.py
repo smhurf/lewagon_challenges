c4fb20e5789d9c68b252ab744f3225b51b1b2815
pierre collinet <pi.collinet@gmail.com>
2019-10-10 11:49:55 +0200

    solve the first exercises -EX01
    Former-commit-id: 64e2b6d3f9fd5d83a90901bdd050380d5426b40d

--- 01-Python/04-SQL-Advanced/01-Query-the-DB/query_db.py
+++ 01-Python/04-SQL-Advanced/01-Query-the-DB/query_db.py
@@ -1,4 +1,8 @@
 # pylint:disable=missing-module-docstring
+import sqlite3
+
+conn = sqlite3.connect('db/ecommerce.db')
+db = conn.cursor()
 
 def query_orders(db):
     """TODO: return a list of orders with each column"""
@@ -9,16 +13,28 @@ def query_orders(db):
 
 def get_orders_range(db, date_from, date_to):
     """TO DO: return a list of orders with each column with OrderDate between date_from to date_to"""
+    t = (date_from, date_to)
     request = '''SELECT * FROM orders
-    where
+    WHERE orders.OrderDate>? and orders.OrderDate<?
     '''
-    results = db.execute(request)
+    results = db.execute(request, t)
     return results
 
 
 def get_waiting_time(db):
-    """TO DO: get a list with all the orders and the timedelta between OrderDate and ShippedDate orders by ascending timedelta"""
-    request = '''
+    """TO DO: get a list with all the orders with each column + and extra TimeDelta column displaying the number of days between OrderDate and ShippedDate orders by ascending timedelta"""
+    request = '''SELECT
+    *,
+    julianday(orders.ShippedDate) - julianday(orders.OrderDate) AS TimeDelta
+    FROM orders
+    ORDER BY TimeDelta ASC
     '''
     results = db.execute(request)
     return results
+
+date_from = "2012-01-04"
+date_to = "2012-05-04"
+#results = get_orders_range(db, date_from, date_to)
+results = get_waiting_time(db)
+results = results.fetchall()
+print(results)
