46b388df3d4991f9b48f3726117623038433bb8b
pierre collinet <pi.collinet@gmail.com>
2019-10-10 11:14:11 +0200

    add exercises in ex01
    Former-commit-id: bc75e0e49c24f5a053683ae750bc417de0df5149

--- 01-Python/04-SQL-Advanced/01-Query-the-DB/query_db.py
+++ 01-Python/04-SQL-Advanced/01-Query-the-DB/query_db.py
@@ -5,3 +5,19 @@ def query_orders(db):
     request = '''SELECT * FROM orders'''
     results = db.execute(request)
     return results
+
+
+def get_orders_range(db, date_from, date_to):
+    """TO DO: orders with OrderDate between date_from to date_to"""
+    request = '''
+    '''
+    results = db.execute(request)
+    return results
+
+
+def get_waiting_time(db):
+    """TO DO: get a list with all the orders and the timedelta between OrderDate and ShippedDate orders by ascending timedelta"""
+    request = '''
+    '''
+    results = db.execute(request)
+    return results
