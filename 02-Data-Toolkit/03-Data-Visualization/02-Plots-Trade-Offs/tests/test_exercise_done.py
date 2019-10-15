a88984fc9ddf97ed31aa7e58090e67b98b912545
GaetanDuron <gaetan.duron@gmail.com>
2019-10-15 13:53:35 +0200

    remove old exercise files + add ex01 readme
    Former-commit-id: fd0db1e78fcea8fc30e101977e99e86c42e2cb48

--- 02-Data-Toolkit/03-Data-Visualization/02-Plots-Trade-Offs/tests/test_exercise_done.py
+++
@@ -1,9 +0,0 @@
-# pylint: disable-all
-
-import unittest
-import os.path
-
-class TestExerciseDone(unittest.TestCase):
-    def test_exercise_done(self):
-        f = os.path.isfile("Exercise_02.ipynb") 
-        self.assertEqual(f, True)
--- 02-Data-Toolkit/03-Data-Visualization/04-Maps/tests/test_exercise_done.py
+++
@@ -1,9 +0,0 @@
-# pylint: disable-all
-
-import unittest
-import os.path
-
-class TestExerciseDone(unittest.TestCase):
-    def test_exercise_done(self):
-        f = os.path.isfile("Exercise_04.ipynb") 
-        self.assertEqual(f, True)
