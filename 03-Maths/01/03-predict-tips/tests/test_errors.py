61bef2f631144f85d2cb5cf54f8f9d19a0ca5074
pierre collinet <pi.collinet@gmail.com>
2019-10-16 17:20:56 +0200

    update ex03
    Former-commit-id: cfd7ea1abffce2eb9efb5c3356d7a3a121cc56e3

--- 03-Maths/01/03-predict-tips/tests/test_errors.py
+++ 03-Maths/01/03-predict-tips/tests/test_errors.py
@@ -2,7 +2,7 @@ import unittest
 from compute_error import get_errors
 
 class TestError(unittest.TestCase):
-    def test_get_erros(self):
+    def squared_errors(self):
         expected = 5
         self.assertEqual(get_errors(), expected)
 
