76a399b23e8f506aa818f569c399bff2baf08bd2
pierre collinet <pi.collinet@gmail.com>
2019-10-16 15:38:45 +0200

    change mispelling word in addition.py
    Former-commit-id: 602139025bce2922c7284c3b6c061ed3a3224706

--- 03-Maths/01/01-linear-algebra/addition.py
+++ 03-Maths/01/01-linear-algebra/addition.py
@@ -2,7 +2,7 @@
 
 from basic_functions import matrix_size
 
-def addition_2_matrices(matrice_1, matrice_2):
+def addition_2_matrices(matrix_1, matrix_2):
     '''TO DO: return the sum of the 2 matrices A and B. If the size are
               not consistent, you should raise a IndexError.'''
     pass
