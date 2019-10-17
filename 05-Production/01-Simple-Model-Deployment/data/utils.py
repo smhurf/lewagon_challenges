a0ac24cf3c255aa395a5f6e2a5d1635da668afb8
jeanb <jean@styckr.io>
2019-10-17 09:11:20 +0200

    minor changes
    Former-commit-id: efe6ad76131cd27a773b095b73466b8eb18dc58b

--- 05-Production/01-Simple-Model-Deployment/data/utils.py
+++ 05-Production/01-Simple-Model-Deployment/data/utils.py
@@ -1,11 +1,21 @@
 import json
 
 import pandas as pd
+
 NAME_JSON = 'test.json'
 test = pd.read_csv('05_Production_TaxiFare_TEST_1000_with_answers.csv')
 
 test = test.drop(['pickup_datetime', 'key', 'fare_amount'], axis=1)
-test_lists = test.values.tolist()
 
-with open(NAME_JSON, 'w') as outfile:
-    json.dump(test_lists, outfile)
+
+def to_txt(test):
+    test.to_csv('test.txt', header=None, index=None, sep=',', mode='a')
+
+
+def to_list(test):
+    test_lists = test.values.tolist()
+
+    with open(NAME_JSON, 'w') as outfile:
+        json.dump(test_lists, outfile)
+
+to_txt(test)
