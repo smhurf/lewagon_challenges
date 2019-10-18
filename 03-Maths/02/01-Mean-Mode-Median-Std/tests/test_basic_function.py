876e1fa0bb1201331e0fabe5fd9ef92ddde317e4
Gaetan Duron <gaetan.duron@gmail.com>
2019-10-18 10:39:05 +0200

    fix make
    Former-commit-id: 04083ada2c55db2c9329670467b72ec60c612bdd

--- 03-Maths/02/01-Mean-Mode-Median-Std/tests/test_basic_function.py
+++ 03-Maths/02/01-Mean-Mode-Median-Std/tests/test_basic_function.py
@@ -3,7 +3,7 @@
 import unittest
 import statistics
 import numpy as np 
-from basic_functions_solution import *
+from basic_functions import *
 
 class TestBasicFunctions(unittest.TestCase):
     def test_mean(self):
