7397d06725d2af321a8e5532eed260dcc2c1a563
GaetanDuron <gaetan.duron@gmail.com>
2019-11-18 12:24:18 +0100

    update test file
    Former-commit-id: a870dbd006148ef7b02f4813a5bf8d3e69d92337

--- 07-Best-Practices/01/02-Data-Cleaning/tests/test_cleaning_data.py
+++ 07-Best-Practices/01/02-Data-Cleaning/tests/test_cleaning_data.py
@@ -1,20 +1,19 @@
 import unittest
 from data_cleaning import missing_data, datetime_columns
 
-
 class TestDataCleaning(unittest.TestCase):
-    def test_missing_data_columns(self):
-        expected = ['review_comment_message', 'review_comment_title',
-                    'order_delivered_carrier_date', 'order_delivered_customer_date'
-                    'product_category_name', 'product_description_lenght',
-                    'product_name_lenght', 'product_photos_qty']
-        test_list = missing_data()
-        self.assertEqual(sorted(test_list), sorted(expexted))
+   def test_missing_data_columns(self):
+       expected = ['review_comment_message', 'review_comment_title',
+                   'order_delivered_carrier_date', 'order_delivered_customer_date',
+                   'product_category_name', 'product_description_lenght',
+                   'product_name_lenght', 'product_photos_qty']
+       test_list = missing_data()
+       self.assertEqual(sorted(test_list), sorted(expected))
 
-    def test_missing_data_columns(self):
-        expected = ['order_delivered_carrier_date',
-                    'order_delivered_customer_date',
-                    'shipping_limit_date', 'review_answer_timestamp',
-                    'review_creation_date']
-        test_list = datetime_columns()
-        self.assertEqual(sorted(test_list), sorted(expected))
+   def test_datetime_columns(self):
+       expected = ['order_estimated_delivery_date',
+                   'order_purchase_timestamp',
+                   'shipping_limit_date', 'review_answer_timestamp',
+                   'review_creation_date']
+       test_list = datetime_columns()
+       self.assertEqual(sorted(test_list), sorted(expected))
