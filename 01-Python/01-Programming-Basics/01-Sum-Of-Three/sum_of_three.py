b5052ca897c89297a6951a07c008fbbd11a28861
Nicolas Bancel <nicolas.bancel@gmail.com>
2020-03-09 15:41:03 +0100

    modifying function
    Former-commit-id: 95225aedd464b8d9e8f48f05054800882550c66b

--- 01-Python/01-Programming-Basics/01-Sum-Of-Three/sum_of_three.py
+++ 01-Python/01-Programming-Basics/01-Sum-Of-Three/sum_of_three.py
@@ -4,4 +4,4 @@ def sum3(first_number, second_number, third_number):
     """
     Takes three number and return the addition of those numbers
     """
-    pass # TODO: implement this function
+    return first_number + second_number + third_number
