6bad30ad9027070f7b6cab518f43f4690c339d1b
Lucien George <luciengeorge95@gmail.com>
2020-05-04 12:50:04 +0100

    added two tests for third method
    Former-commit-id: 40ad6b393e1c41c0bfefeabff80a059882e2768a

--- 01-Python/02-Data-Sourcing/01-CSV/tests/test_winter_olympic_games.py
+++ 01-Python/02-Data-Sourcing/01-CSV/tests/test_winter_olympic_games.py
@@ -17,3 +17,11 @@ class TestWinterOlympicGames(unittest.TestCase):
     def test_country_with_most_gold_medals_between_1994_and_1998(self):
         country = country_with_most_gold_medals(1994, 1998)
         self.assertEqual(country, 'Germany')
+
+    def test_top_five_women_super_g_gold_medals_above_1994(self):
+      women = top_five_women_super_g_gold_medals(1994)
+      self.assertEqual(women, ['MEISSNITZER, Alexandra', 'DORFMEISTER, Michaela', 'KOSTELIC, Janica', 'KOSTNER, Isolde', 'ROFFE, Diann'])
+
+    def test_top_five_women_super_g_gold_medals_above_2010(self):
+      women = top_five_women_super_g_gold_medals(2010)
+      self.assertEqual(women, ['VONN, Lindsey', 'FISCHBACHER, Andrea', 'MAZE, Tina', 'FENNINGER, Anna', 'HOSP, Nicole'])
