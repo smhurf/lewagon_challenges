cb58e363209a71069c68cabfd612bd9706b187a1
kevin robert <kevin.j.robert@gmail.com>
2020-05-12 18:07:18 +0200

    update classes
    Former-commit-id: 3c21f8f11adb4681c4903da09c2c7346fc428f82

--- 04-Decision-Science/olist/order.py
+++ 04-Decision-Science/olist/order.py
@@ -6,20 +6,24 @@ from olist.data import Olist
 
 
 class Order:
+    '''
+    DataFrames containing all orders delivered as index, and various properties of these orders as columns
+    '''
 
     def __init__(self):
         self.data = Olist().get_data()
 
-    def get_wait_time(self):
+    def get_wait_time(self, is_delivered=True):
         """
         02-01 > Returns a DataFrame with:
-        order_id, wait_time, expected_wait_time ,delay_vs_expected
+        [order_id, wait_time, expected_wait_time ,delay_vs_expected]
+        and filtering out non-delivered orders unless specified
         """
 
     def get_review_score(self):
         """
         02-01 > Returns a DataFrame with:
-        order_id, dim_is_five_star, dim_is_one_star, review_score
+        order_id, dim_is_five_star, dim_is_one_star
         """
 
     def get_number_products(self):
@@ -48,10 +52,10 @@ class Order:
         # Optional
         # Hint: you can use the haversine_distance logic coded in olist/utils.py
 
-    def get_training_data(self):
+    def get_training_data(self, is_delivered=True):
         """
-        02-01 > Returns a DataFrame with:
-        order_id, wait_time, wait_vs_expected,
+        02-01 > Returns a clean DataFrame (without NaN), with the following columns:
+        [order_id, wait_time, wait_vs_expected,
         dim_is_five_star, dim_is_one_star, review_score, number_of_products,
-        number_of_sellers, freight_value, distance_customer_seller
+        number_of_sellers, freight_value, distance_customer_seller]
         """
