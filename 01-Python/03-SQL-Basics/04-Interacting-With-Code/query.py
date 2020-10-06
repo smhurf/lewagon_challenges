5f408205b4d78e706543ad78dceb5e1ed2ffb187
kevin robert <kevin.j.robert@gmail.com>
2020-10-06 15:38:33 +0200

    introduce yaml for results
    Former-commit-id: b7ab51dc494c1a5ec1b9e382949bf7f1e2ab3d91

---
+++ 01-Python/03-SQL-Basics/04-Interacting-With-Code/query.py
@@ -0,0 +1,27 @@
+# pylint: disable=missing-docstring, C0103
+
+
+def directors_count(db):
+    # TO-DO: use 'db' to execute an SQL query against the database.
+    # Return directors count in database.
+    pass
+
+def sorted_directors(db):
+    # TO-DO: return a list of directors' names sorted alphabetically
+    pass
+
+def love_movies(db):
+    """TO-DO: return a list of movies with love in the names, sorted
+    alphabetically"""
+    pass
+
+
+def directors_with_name(db, name):
+    # TO-DO: count number of director with this name
+    pass
+
+
+def long_movies(db, min_length):
+    """TO-DO: return a list of movies' names
+    verifying: minutes > min_length, sorted by length (ascending)"""
+    pass
