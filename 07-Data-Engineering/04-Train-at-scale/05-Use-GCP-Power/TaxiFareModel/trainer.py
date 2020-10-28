2d7984073db489e42b2b319ecad4561ad8681042
Gaetan Manchon <gmanchon@gmail.com>
2020-10-28 16:13:16 +0100

    replace le wagon mlflow server url
    Former-commit-id: 9ada5ec2d6990bc76828f11b05bb8fc3bd37add8

--- 07-Data-Engineering/04-Train-at-scale/05-Use-GCP-Power/TaxiFareModel/trainer.py
+++ 07-Data-Engineering/04-Train-at-scale/05-Use-GCP-Power/TaxiFareModel/trainer.py
@@ -29,7 +29,7 @@ from TaxiFareModel.utils import compute_rmse, simple_time_tracker
 from TaxiFareModel.params import MODEL_VERSION
 
 # Mlflow wagon server
-MLFLOW_URI = "http://35.210.166.253:5000"
+MLFLOW_URI = "https://mlflow.lewagon.co/"
 
 
 class Trainer(object):
