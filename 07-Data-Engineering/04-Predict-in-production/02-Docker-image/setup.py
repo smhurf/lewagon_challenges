6b8c48db16004720b0cd871ceb57addcf2465533
Gaetan Manchon <gmanchon@gmail.com>
2020-11-25 15:56:36 +0100

    update setup in order to load packages from requirements
    Former-commit-id: 93197fb2c811c12d0d50e8af69addae016f15abe

--- 07-Data-Engineering/04-Predict-in-production/02-Docker-image/setup.py
+++ 07-Data-Engineering/04-Predict-in-production/02-Docker-image/setup.py
@@ -1,30 +1,12 @@
-from setuptools import find_packages
-from setuptools import setup
+from setuptools import setup, find_packages
 
-REQUIRED_PACKAGES = [
-    'gcsfs==0.6.0',
-    'pandas==0.24.2',
-    'scikit-learn==0.20.4',
-    'google-cloud-storage==1.26.0',
-    'pygeohash',
-    'category_encoders',
-    'mlflow==1.8.0',
-    'joblib==0.14.1',
-    'numpy==1.18.4',
-    'psutil==5.7.0',
-    'pygeohash==1.2.0',
-    'termcolor==1.1.0',
-    'xgboost==1.1.1',
-    'memoized-property==1.0.3',
-    'scipy== 1.2.2',
-    'category_encoders==2.2.2',
-    's3fs']
+with open("requirements.txt") as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content]
 
-setup(
-    name='TaxiFareModel',
-    version='1.0',
-    install_requires=REQUIRED_PACKAGES,
-    packages=find_packages(),
-    include_package_data=True,
-    description='Taxi Fare Prediction Pipeline'
-)
+setup(name="TaxiFareModel",
+      version="1.0",
+      description="Package description",
+      packages=find_packages(),
+      include_package_data=True,  # includes in package files from MANIFEST.in
+      install_requires=requirements)
