adec4af4ec8c4f849806c69db29a77a3adf9854a
Gaetan Manchon <gmanchon@gmail.com>
2020-12-01 17:14:05 +0100

    D3X2 rename TaxiFareModel package to SimpleTaxiFare because this exercice is standalone
    Former-commit-id: b35cd07e829cd130141c138136ea2bcf1adde2f5

--- 07-Data-Engineering/03-Train-at-scale/02-Submit-training-tasks/setup.py
+++ 07-Data-Engineering/03-Train-at-scale/02-Submit-training-tasks/setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 requirements = [x.strip() for x in content if 'git+' not in x]
 
 setup(
-    name='TaxiFareModel',
+    name='SimpleTaxiFare',
     version='1.0',
     install_requires=requirements,
     packages=find_packages(),
