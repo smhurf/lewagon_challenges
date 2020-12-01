2e12909b92e3b554f20f23af8c24c651319f06cf
Gaetan Manchon <gmanchon@gmail.com>
2020-12-01 17:37:58 +0100

    modify setup.py
    Former-commit-id: 14d07929b8430fcb58428a4f9a25b75e6f580d2f

--- 07-Data-Engineering/03-Train-at-scale/02-Submit-training-tasks/setup.py
+++ 07-Data-Engineering/03-Train-at-scale/02-Submit-training-tasks/setup.py
@@ -1,36 +1,14 @@
 from setuptools import find_packages
 from setuptools import setup
 
-REQUIRED_PACKAGES = [
-    'pip>=9',
-    'setuptools>=26',
-    'wheel>=0.29',
-    'pandas',
-    'pytest',
-    'coverage',
-    'flake8',
-    'black',
-    'yapf',
-    'python-gitlab',
-    'twine',
-    'six==1.14.0',
-    'numpy==1.18.5',
-    'pandas',
-    'scikit-learn==0.22',
-    'joblib',
-    'memoized-property',
-    'mlflow',
-    's3fs',
-    'gcsfs',
-    'google-cloud-storage',
-    'termcolor'
-]
+with open('requirements.txt') as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content if 'git+' not in x]
 
-setup(
-    name='SimpleTaxiFare',
-    version='1.0',
-    install_requires=REQUIRED_PACKAGES,
-    packages=find_packages(),
-    include_package_data=True,
-    description='Taxi Fare Prediction Pipeline'
-)
+setup(name='SimpleTaxiFare',
+      version="1.0",
+      description="Project Description",
+      packages=find_packages(),
+      # include_package_data: to install data from MANIFEST.in
+      include_package_data=True,
+      install_requires=requirements)
