25713f331cba5e602702b002e2780af5ee8cace8
kevin robert <kevin.j.robert@gmail.com>
2020-12-28 12:42:20 +0100

    programming basics
    Former-commit-id: fca17ebb2a7557468eb8264c893313bac8b409ac

--- 01-Python/01-Programming-Basics/01-Sum-Of-Three/tests/test_sum_of_three.py
+++ 01-Python/01-Programming-Basics/01-Sum-Of-Three/tests/test_sum_of_three.py
@@ -3,6 +3,7 @@
 import unittest
 from sum_of_three import sum3
 
+
 class TestSumOfThree(unittest.TestCase):
     def test_numbers_0_0_0(self):
         self.assertEqual(sum3(0, 0, 0), 0)
--- 01-Python/01-Programming-Basics/03-Environment/tests/test_flask_option.py
+++ 01-Python/01-Programming-Basics/03-Environment/tests/test_flask_option.py
@@ -4,6 +4,7 @@ import unittest
 import os
 from flask_option import start
 
+
 class TestFlaskOption(unittest.TestCase):
     def test_start_with_flask_env_development(self):
         os.environ['FLASK_ENV'] = 'development'
