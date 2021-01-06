36e1dff0e8ba88d8e6eb4462db10795f1a6e7750
Damien Milon <37899667+dmilon@users.noreply.github.com>
2021-01-06 16:46:50 +0100

    remove solutions
    Former-commit-id: dc5128d6944cdce95d2f72bb3c31451f9bf8aec2

--- 01-Python/03-SQL-Basics/04-Interacting-With-Code/queries.py
+++ 01-Python/03-SQL-Basics/04-Interacting-With-Code/queries.py
@@ -2,55 +2,23 @@
 
 def directors_count(db):
     # Return the number of directors contained in the database
-    query = """
-        SELECT COUNT(*)
-        FROM directors
-    """
-    result = db.execute(query).fetchone()
-    return result[0]
-
+    pass
 
 def directors_list(db):
     # Return the list of all the directors sorted in alphabetical order
-    query = """
-        SELECT name
-        FROM directors
-        ORDER BY name
-    """
-    results = db.execute(query).fetchall()
-    return [element[0] for element in results]
+    pass
 
 
 def love_movies(db):
-    # Return the list of all movies which contain the word "love" in their title, sorted in alphabetical order
-    query = """
-        SELECT title
-        FROM movies
-        WHERE title LIKE '%love%'
-        ORDER BY title
-    """
-    results = db.execute(query).fetchall()
-    return [element[0] for element in results]
+    # return the list of all movies which contain the word "love" in their title, sorted in alphabetical order
+    pass
 
 
 def directors_named_like_count(db, name):
-    # Return the number of directors which contain a given word in their name
-    query = """
-        SELECT COUNT(*)
-        FROM directors
-        WHERE name LIKE ?
-    """
-    result = db.execute(query, (f"%{name}%",)).fetchone()
-    return result[0]
+    # return the number of directors which contain a given word in their name
+    pass
 
 
 def movies_longer_than(db, min_length):
-    # Return this list of all movies which are longer than a given duration, sorted in the alphabetical order
-    query = """
-        SELECT title
-        FROM movies
-        WHERE minutes > ?
-        ORDER BY minutes
-    """
-    results = db.execute(query, (min_length,)).fetchall()
-    return [element[0] for element in results]
+    # return this list of all movies which are longer than a given duration, sorted in the alphabetical order
+    pass
--- 01-Python/03-SQL-Basics/05-SQL-Queries/queries.py
+++ 01-Python/03-SQL-Basics/05-SQL-Queries/queries.py
@@ -2,79 +2,24 @@
 
 def detailed_movies(db):
     # return the list of movies with their genres and director name
-    query = """
-        SELECT movies.title, movies.genres, directors.name
-        FROM movies
-        JOIN directors ON movies.director_id = directors.id
-    """
-    return db.execute(query).fetchall()
+    pass
 
 def top_five_youngest_newly_directors(db):
     # return the top 5 youngest directors when they direct their first movie
-    query = """
-        SELECT
-            directors.name,
-            movies.start_year - directors.birth_year age
-        FROM directors
-        JOIN movies ON directors.id = movies.director_id
-        WHERE age IS NOT NULL
-        ORDER BY age
-        LIMIT 5
-    """
-    return db.execute(query).fetchall()
+    pass
 
 def late_released_movies(db):
     # return the list of all movies released after their director death
-    query = """
-        SELECT movies.title
-        FROM directors
-        JOIN movies ON directors.id = movies.director_id
-        WHERE (movies.start_year - directors.death_year) > 0
-        ORDER BY movies.title
-    """
-    results = db.execute(query).fetchall()
-    return [movie[0] for movie in results]
+    pass
 
 def stats_on(db, genre_name):
     # return a dict of stats for a given genre
-    query = """
-        SELECT
-            COUNT(*),
-            AVG(minutes)
-        FROM movies
-        WHERE genres = ?
-    """
-    result = db.execute(query, (genre_name,)).fetchone()
-    return {
-        "genre": genre_name,
-        "number_of_movies": result[0],
-        "avg_length": round(result[1], 2)
-    }
-
+    pass
 
 def top_five_directors_for(db, genre_name):
     # return the top 5 of the directors with the most movies for a given genre
-    query = """
-        SELECT
-            directors.name,
-            COUNT(*) movie_count
-        FROM movies
-        JOIN directors ON movies.director_id = directors.id
-        WHERE genres = ?
-        GROUP BY directors.name
-        ORDER BY movie_count DESC, directors.name
-        LIMIT 5
-    """
-    return db.execute(query, (genre_name,)).fetchall()
+    pass
 
 def movie_duration_buckets(db):
     # return the movie counts grouped by bucket of 30 min duration
-    query = """
-        SELECT
-            (minutes / 30 + 1)*30 time_range,
-            COUNT(*)
-        FROM movies
-        WHERE minutes IS NOT NULL
-        GROUP BY time_range
-    """
-    return db.execute(query).fetchall()
+    pass
