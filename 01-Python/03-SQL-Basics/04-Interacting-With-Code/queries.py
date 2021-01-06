1e556bddf9616e22b80aaacd7ad90f7d537feedf
Damien Milon <37899667+dmilon@users.noreply.github.com>
2021-01-06 07:47:56 +0100

    update sql basics challenge 4
    Former-commit-id: d17cbd8daa6a79d5e425ced1efa642fc9e88056b

--- 01-Python/03-SQL-Basics/04-Interacting-With-Code/queries.py
+++ 01-Python/03-SQL-Basics/04-Interacting-With-Code/queries.py
@@ -4,26 +4,57 @@
 def directors_count(db):
     # TO-DO: use 'db' to execute an SQL query against the database.
     # Return directors count in database.
-    pass
+    query = """
+        SELECT COUNT(*)
+        FROM directors
+    """
+    result = db.execute(query).fetchone()
+    return result[0]
 
 
 def sorted_directors(db):
     # TO-DO: return a list of directors' names sorted alphabetically
-    pass
+    query = """
+        SELECT name
+        FROM directors
+        ORDER BY name
+    """
+    results = db.execute(query).fetchall()
+    return [element[0] for element in results]
 
 
 def love_movies(db):
     """TO-DO: return a list of movies with 'love' in the name, sorted
     alphabetically"""
-    pass
+    query = """
+        SELECT title
+        FROM movies
+        WHERE title LIKE '%love%'
+        ORDER BY title
+    """
+    results = db.execute(query).fetchall()
+    return [element[0] for element in results]
 
 
 def directors_with_name(db, name):
-    # TO-DO: count the number of directors with this name
-    pass
+    # TO-DO: count the number of directors with a name like `name`
+    query = """
+        SELECT COUNT(*)
+        FROM directors
+        WHERE name LIKE ?
+    """
+    result = db.execute(query, (f"%{name}%",)).fetchone()
+    return result[0]
 
 
 def long_movies(db, min_length):
     """TO-DO: return a list of movie names
     verifying: minutes > min_length, sorted by length (ascending)"""
-    pass
+    query = """
+        SELECT title
+        FROM movies
+        WHERE minutes > ?
+        ORDER BY minutes
+    """
+    results = db.execute(query, (min_length,)).fetchall()
+    return [element[0] for element in results]
