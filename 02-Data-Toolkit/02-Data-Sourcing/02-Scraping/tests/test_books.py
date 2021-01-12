7aedae174fcbcb1adfdc74d106f27075ec46a823
kevin robert <kevin.j.robert@gmail.com>
2021-01-12 16:35:02 +0100

    make scrapping tests more robust
    Former-commit-id: f7b314f105876bb7b91d42048c59834d996190fd

--- 02-Data-Toolkit/02-Data-Sourcing/02-Scraping/tests/test_books.py
+++ 02-Data-Toolkit/02-Data-Sourcing/02-Scraping/tests/test_books.py
@@ -5,8 +5,8 @@ class TestBooks(ChallengeResultTestCase):
 
     def test_books_dict_id_defined(self):
         self.assertEqual(
-            self.result.books_dict,
-            {'Title': [], 'Price': [], 'Rating': []}
+            list(self.result.books_dict.keys()),
+            ['Title', 'Price', 'Rating']
         )
 
     def test_books_df_columns_name(self):
@@ -19,7 +19,7 @@ class TestBooks(ChallengeResultTestCase):
         self.assertEqual(len(list(self.result.columns)), 3)
 
     def test_first_book_title(self):
-        self.assertEqual(self.result.title, 'A Light in the ...')
+        self.assertTrue('A Light in the' in self.result.title)
 
     def test_first_book_price(self):
         self.assertEqual(self.result.price, 51.77)
