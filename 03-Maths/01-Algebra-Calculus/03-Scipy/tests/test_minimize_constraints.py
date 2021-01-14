6240f874995ac771d4365b63eba79aba9cf75c8d
kevin robert <kevin.j.robert@gmail.com>
2021-01-14 18:44:23 +0100

    lighter tests optimization
    Former-commit-id: 5da733ebe70073f9a2479679c644d4a54c72c403

--- 03-Maths/01-Algebra-Calculus/03-Scipy/tests/test_minimize_constraints.py
+++ 03-Maths/01-Algebra-Calculus/03-Scipy/tests/test_minimize_constraints.py
@@ -4,35 +4,38 @@ import numpy as np
 
 class TestMinimizeConstraints(ChallengeResultTestCase):
     def test_starting_point_shape(self):
-        self.assertEqual(self.result.X0.shape, (4,))
-
-    def test_bounds_shape(self):
-        self.assertEqual(len(self.result.bounds), 4)
-
-    def test_bounds_is_tuple(self):
-        self.assertIsInstance(self.result.bounds, tuple)
-
-    def test_bounds_elements_are_tuples(self):
-        for bound in self.result.bounds:
-            self.assertIsInstance(bound, tuple)
+        self.assertEqual(
+            self.result.X0.shape,
+            (4,),
+            'X0 should be a (4,) ndarray'
+        )
 
     def test_bounds_values(self):
         for bound in self.result.bounds:
-            self.assertEqual(bound, (1, 5))
+            self.assertEqual(
+                bound,
+                (1, 5),
+                'Boundaries should be between 1 and 5'
+            )
 
     def test_minimum_shape(self):
-        self.assertEqual(self.result.Xmin.shape, (4,))
+        self.assertEqual(
+            self.result.Xmin.shape,
+            (4,),
+            'The local minimum should be a (4,) ndarray'
+        )
 
     def test_first_constraint(self):
         first_constraint = np.sum(self.result.Xmin ** 2)
-        self.assertAlmostEqual(first_constraint, 40)
+        self.assertGreater(first_constraint, 39)
+        self.assertGreater(41, first_constraint)
 
     def test_second_constraint(self):
         Xmin = self.result.Xmin
         second_constraint = Xmin[0] * Xmin[1] * Xmin[2] * Xmin[3]
-        self.assertGreater(25, second_constraint)
+        self.assertGreaterEqual(25, second_constraint)
 
     def test_third_constraint(self):
         Xmin = self.result.Xmin
-        self.assertTrue(np.all(Xmin <= 5))
-        self.assertTrue(np.all(Xmin >= 1))
+        self.assertTrue(np.all(Xmin <= 5), 'Make sure the upper boundary is 5')
+        self.assertTrue(np.all(Xmin >= 1), 'Make sure the lower boundary is 1')
