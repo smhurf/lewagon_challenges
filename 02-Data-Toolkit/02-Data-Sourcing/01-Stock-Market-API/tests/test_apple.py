bd32a5f4647b18e42f8b3157b4e3155287ac4f6e
Chris Westerman <chris.westerman8@gmail.com>
2021-01-19 12:46:28 +0000

    updated column test to remove order comparison
    Former-commit-id: decdfa35707036df3e2b123d3e0dd577ca92e41c

--- 02-Data-Toolkit/02-Data-Sourcing/01-Stock-Market-API/tests/test_apple.py
+++ 02-Data-Toolkit/02-Data-Sourcing/01-Stock-Market-API/tests/test_apple.py
@@ -10,13 +10,11 @@ class TestApple(ChallengeResultTestCase):
         self.assertEqual(self.result.index_type, '<M8[ns]', "Check you converted the 'date' colummn into a `datetime`.")
 
     def test_apple_df_columns(self):
-        self.assertEqual(
-            list(self.result.columns),
-            sorted([
-                'close', 'high', 'low', 'open', 'symbol', 'volume', 'id',
-                'key', 'subkey', 'updated', 'changeOverTime',
-                'marketChangeOverTime', 'uOpen', 'uClose', 'uHigh', 'uLow',
-                'uVolume', 'fOpen', 'fClose', 'fHigh', 'fLow', 'fVolume',
-                'label', 'change', 'changePercent'
-            ])
-        )
+        columns = {
+            'close', 'high', 'low', 'open', 'symbol', 'volume', 'id',
+            'key', 'subkey', 'updated', 'changeOverTime',
+            'marketChangeOverTime', 'uOpen', 'uClose', 'uHigh', 'uLow',
+            'uVolume', 'fOpen', 'fClose', 'fHigh', 'fLow', 'fVolume',
+            'label', 'change', 'changePercent'
+            }
+        self.assertEqual(columns.issubset(set(self.result.columns)), True)
