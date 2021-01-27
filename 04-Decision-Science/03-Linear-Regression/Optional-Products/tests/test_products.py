8b3ecb6f2bc9a65016ab3e02714fb0d23a842439
Chris Westerman <chris.westerman8@gmail.com>
2021-01-27 10:12:42 +0000

    4-3-OPROD cleaned up instructions in jupyter / sublime, adjusted tests
    Former-commit-id: 6fcf291177f9b8fb754e6e2b08e363f0b5378bc4

--- 04-Decision-Science/03-Linear-Regression/Optional-Products/tests/test_products.py
+++ 04-Decision-Science/03-Linear-Regression/Optional-Products/tests/test_products.py
@@ -3,7 +3,7 @@ from nbresult import ChallengeResultTestCase
 
 class TestProducts(ChallengeResultTestCase):
   def test_shape(self):
-    self.assertEqual(self.result.shape, (71, 12))
+    self.assertEqual(self.result.shape, (71, 15))
 
   def test_average_review_score(self):
     self.assertEqual(self.result.avg_review_score, 4)
@@ -12,4 +12,4 @@ class TestProducts(ChallengeResultTestCase):
     self.assertEqual(self.result.avg_price, 169)
 
   def test_average_quantity(self):
-    self.assertEqual(self.result.avg_quantity, 1560)
+    self.assertEqual(self.result.avg_quantity, 1551)
