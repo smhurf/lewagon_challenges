5216768266c55ef028a255e26baa740244166879
Chris Westerman <chris.westerman8@gmail.com>
2021-02-02 10:16:18 +0000

    5-3-2 updated thresholds on model score type tests and fixed typos
    Former-commit-id: 880efb4682d8659ea8337e60322dae003550bc5e

--- 05-ML/03-Performance-metrics/02-Electrocardiograms/tests/test_logistic_regression_evaluation.py
+++ 05-ML/03-Performance-metrics/02-Electrocardiograms/tests/test_logistic_regression_evaluation.py
@@ -6,10 +6,12 @@ class TestLogistic_regression_evaluation(ChallengeResultTestCase):
         self.assertGreater(self.result.accuracy, 0.85)
 
     def test_recall(self):
-        self.assertLess(self.result.recall, 0.5)
+        self.assertLess(self.result.recall, 0.4)
 
     def test_precision(self):
         self.assertLess(self.result.precision, 0.8)
+        self.assertGreater(self.result.precision, 0.5)
 
     def test_f1(self):
         self.assertLess(self.result.f1, 0.6)
+        self.assertGreater(self.result.f1, 0.4)
