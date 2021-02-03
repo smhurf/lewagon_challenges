a924dffdb187c712e4e95ac62445fa160561556f
kevin robert <kevin.j.robert@gmail.com>
2021-02-03 15:05:27 +0100

    remove extra files tests
    Former-commit-id: 4fe1de25b24b06aee772b916ecbd68d4588b7624

--- 05-ML/06-Unsupervised-Learning/01-Image-Compression/tests/test_colorcount.py
+++
@@ -1,6 +0,0 @@
-from nbresult import ChallengeResultTestCase
-
-
-class TestColorcount(ChallengeResultTestCase):
-    def test_colorcount(self):
-        self.assertEqual(self.result.color_count, 113382)
