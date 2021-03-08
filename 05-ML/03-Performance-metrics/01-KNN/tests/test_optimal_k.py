41196c573c650cdf866902e05dca7361eb91d32a
Chris Westerman <chris.westerman8@gmail.com>
2021-03-08 11:10:27 +0000

    5-3-1 updated optimal k test to work with 10 fold cv
    Former-commit-id: d8fdd735f3b6525d560c76042e9425a825d28fcc

--- 05-ML/03-Performance-metrics/01-KNN/tests/test_optimal_k.py
+++ 05-ML/03-Performance-metrics/01-KNN/tests/test_optimal_k.py
@@ -4,4 +4,4 @@ from nbresult import ChallengeResultTestCase
 class TestOptimal_k(ChallengeResultTestCase):
     def test_optimal_K_around_10(self):
         self.assertGreaterEqual(self.result.optimal_k, 5)
-        self.assertLessEqual(self.result.optimal_k, 11)
+        self.assertLessEqual(self.result.optimal_k, 14)
