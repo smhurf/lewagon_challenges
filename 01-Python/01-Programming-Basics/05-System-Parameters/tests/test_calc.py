3ab86e8c490e00c25436d05bd4e755703d0d6ea0
Gaetan Manchon <gmanchon@gmail.com>
2021-05-05 14:28:00 +0200

    lint

--- 01-Python/01-Programming-Basics/05-System-Parameters/tests/test_calc.py
+++ 01-Python/01-Programming-Basics/05-System-Parameters/tests/test_calc.py
@@ -6,13 +6,13 @@ from calc import main
 
 class TestCalc(unittest.TestCase):
     def test_4_plus_5(self):
-        sys.argv = [ "", "4", "+", "5"]
+        sys.argv = ["", "4", "+", "5"]
         self.assertEqual(main(), 9)
 
     def test_2_times_6(self):
-        sys.argv = [ "", "2", "*", "6"]
+        sys.argv = ["", "2", "*", "6"]
         self.assertEqual(main(), 12)
 
     def test_3_minus_9(self):
-        sys.argv = [ "", "3", "-", "9"]
+        sys.argv = ["", "3", "-", "9"]
         self.assertEqual(main(), -6)
